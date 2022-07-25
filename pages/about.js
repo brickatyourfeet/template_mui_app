1c822ddebdf31dcb22f08f7c69e50ff7090f0243
brickatyourfeet <brickatyourfeet@gmail.com>
2022-07-24 20:55:45 -0700

    remove unused components from about and reorg page

--- pages/about.js
+++ pages/about.js
@@ -79,9 +79,91 @@ export default function About(props) {
           align="center"
           className={classes.missionStatement}
         >
-          Our mission is to create herbal medicines that are not only effective and affordable, but are the highest possible quality. We support this idea by hand-harvesting almost all of the herbs that we use in our blends from the wild forests of the Pacific Northwest, and by utilizing over 15 years of knowledge and experience in herbalism to create the most effective blends possible. 
+          Our mission is to create herbal medicines that are not only effective and affordable, but are the highest possible quality. We support this idea by hand-harvesting almost all of the herbs that we use in our blends from the wild forests of the Pacific Northwest, and by utilizing over 15 years of knowledge and experience in herbalism to create the most effective blends possible. 
         </Typography>
       </Grid>
+
+
+
+
+
+      <Grid
+        item
+        container
+        className={classes.rowContainer}
+        style={{ marginTop: "10em", marginBottom: "10em" }}
+        direction={matchesMD ? "column" : "row"}
+        alignItems={matchesMD ? "center" : undefined}
+        justifyContent="space-between"
+      >
+
+
+        <Grid item>
+        <Grid item>
+          <Typography align="center" variant="h4" gutterBottom>
+            The Herbalist
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" paragraph align="center">
+            Jewell Braden
+          </Typography>
+          <Typography variant="body1" paragraph align="center">
+            Herbalist - Wildcrafter - Formulator
+          </Typography>
+        </Grid>
+        <Grid item container justifyContent="center" lg>
+          <Avatar alt="avatar" src="/assets/jewell.jpg" className={classes.avatar} style={{maxHeight: matchesMD ? 350 : "44em", filter: "grayscale(100%)"}} />
+        </Grid>
+        </Grid>
+
+{/* move text to here in its own container */}
+
+<Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            style={{ maxWidth: "35em" }}
+          >
+            <Grid item>
+              <Typography
+                align={"center"}
+                variant="h4"
+                gutterBottom
+              >
+                About
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body1"
+                align={"center"}
+                paragraph
+              >
+                              Hiya from Rainier Elixirs! I've been studying herbalism for over 15 years and enjoy wildcrafting the freshest & healthiest herbs to offer in our blends.
+
+Since 2007, I?ve completed herbal courses with Jane Bothwell, Rosemary Gladstar, and Michael & Leslie Tierra, as well as studied with Lynda Emashowski in the ways of native & folk herbalism. I also hold a Bachelor of Science in Alternative Medicine.
+
+I live in the Pacific Northwest with my husband & two kids, and we are lucky enough to be surrounded by lush, bountiful forests in the mountains, lowlands & coastlines of our region. Outings & vacations most often include lots of wild herb hunting!
+              </Typography>
+              <Grid item>
+
+            </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+
+
+      </Grid>
+
+
+
+
+
+
+
       <Grid
         item
         container
@@ -114,7 +196,7 @@ export default function About(props) {
                 align={"center"}
                 paragraph
               >
-                Small batches mean that we control exactly what goes into each blend, and are able to produce them using the most effective methods. This means higher quality herbs & solvents (wild & live), longer macerating times (up to a year), and the ability to use cold press extraction (which squeezes out every last drop of herbal gold). 
+                Small batches mean that we control exactly what goes into each blend, and are able to produce them using the most effective methods. This means higher quality herbs & solvents (wild & live), longer macerating times (up to a year), and the ability to use cold press extraction (which squeezes out every last drop of herbal gold). 
               </Typography>
               <Grid item>
               <Typography
@@ -129,7 +211,7 @@ export default function About(props) {
                 align={"center"}
                 paragraph
               >
-               When plants are dried, they lose many of their nutrients and all of their vitality as a result. Furthermore, the longer a plant remains dried before it is used as medicine, the more nutrients it will lose. Using live plants allows the extraction of almost all of the plant?s nutrients and vital energy into the solution & can be seen as an encapsulation of the entire plant. 
+               When plants are dried, they lose many of their nutrients and all of their vitality as a result. Furthermore, the longer a plant remains dried before it is used as medicine, the more nutrients it will lose. Using live plants allows the extraction of almost all of the plant?s nutrients and vital energy into the solution & can be seen as an encapsulation of the entire plant. 
               </Typography>
               <Typography
                 align={"center"}
@@ -143,7 +225,7 @@ export default function About(props) {
                 align={"center"}
                 paragraph
               >
-            Wild plants in their natural habitat are able to absorb their desired nutrients from their surroundings as nature intended, creating a well-rounded herb with the highest amount of nutrients possible. Farmed plants - while they have their place in herbalism when wild plants are unavailable - lack most of their natural food sources found in their wild habitat, and therefore are lacking in nutrients compared to their wild counterparts. 
+            Wild plants in their natural habitat are able to absorb their desired nutrients from their surroundings as nature intended, creating a well-rounded herb with the highest amount of nutrients possible. Farmed plants - while they have their place in herbalism when wild plants are unavailable - lack most of their natural food sources found in their wild habitat, and therefore are lacking in nutrients compared to their wild counterparts. 
             Every drop is chock-full of vital plant nutrients and energy to elicit deep healing and nourishment.              </Typography>
             </Grid>
             </Grid>
@@ -159,104 +241,7 @@ export default function About(props) {
           </Grid>
         </Grid>
       </Grid>
-      <Grid
-        item
-        container
-        direction="column"
-        alignItems="center"
-        className={classes.rowContainer}
-        style={{ marginBottom: "15em" }}
-      >
-        <Grid item>
-          <Typography align="center" variant="h4" gutterBottom>
-            The Herbalist
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography variant="body1" paragraph align="center">
-            Jewell Braden
-          </Typography>
-          <Typography variant="body1" paragraph align="center">
-            Herbalist - Wildcrafter - Formulator
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Avatar alt="avatar" src="/assets/jewell.jpg" className={classes.avatar} />
-        </Grid>
-        {/** 3 item container below avatar for other potential ideas **/}
-        <Grid item container justifyContent={matchesMD ? "center" : undefined}>
-          <Hidden lgUp> {/** hidden on large screens **/}
-            <Grid item lg style={{ maxWidth: "45em", padding: "1.25em" }}>
-              <Typography variant="body1" align="center" paragraph>
-              Hi! I?m Jewell, the owner & herbalist behind Rainier Elixirs. I've been studying herbalism for over 15 years and enjoy wildcrafting the freshest & healthiest herbs to offer in our blends.
 
-Since 2007, I?ve completed herbal courses with Jane Bothwell, Rosemary Gladstar, and Michael & Leslie Tierra, as well as studied with Lynda Emashowski in the ways of native & folk herbalism. I also hold a Bachelor of Science in Alternative Medicine.
-
-I live in the Pacific Northwest with my husband & two kids, and we are lucky enough to be surrounded by lush, bountiful forests in the mountains, lowlands & coastlines of our region. Outings & vacations most often include lots of wild herb hunting!
-              </Typography>
-              <Typography variant="body1" align="center" paragraph>
-                Lg Up Short blurb maybe if you want that
-              </Typography>
-            </Grid>
-          </Hidden>
-          <Grid
-            item
-            container
-            direction="column"
-            lg
-            alignItems={matchesMD ? "center" : undefined}
-            style={{ marginBottom: matchesMD ? "2.5em" : 0 }}
-          >
-            <Grid item>
-              <img
-                src="/assets/herbs.svg"
-                alt="imagalt"
-                style={{ maxWidth: matchesMD ? 300 : undefined }}
-              />
-            </Grid>
-            <Grid item>
-              <Typography variant="caption">
-                Lg Up brief descript caption flex end
-              </Typography>
-            </Grid>
-          </Grid>
-          <Hidden mdDown> {/** hidden on medium and down screens **/}
-            <Grid item lg style={{ maxWidth: "45em", padding: "1.25em" }}>
-              <Typography variant="body1" align="center" paragraph>
-              Hiya from Rainier Elixirs! I've been studying herbalism for over 15 years and enjoy wildcrafting the freshest & healthiest herbs to offer in our blends.
-
-Since 2007, I?ve completed herbal courses with Jane Bothwell, Rosemary Gladstar, and Michael & Leslie Tierra, as well as studied with Lynda Emashowski in the ways of native & folk herbalism. I also hold a Bachelor of Science in Alternative Medicine.
-
-I live in the Pacific Northwest with my husband & two kids, and we are lucky enough to be surrounded by lush, bountiful forests in the mountains, lowlands & coastlines of our region. Outings & vacations most often include lots of wild herb hunting!
-              </Typography>
-              <Typography variant="body1" align="center" paragraph>
-                MD Down Short blurb maybe if you want that
-              </Typography>
-            </Grid>
-          </Hidden>
-          <Grid
-            item
-            container
-            direction="column"
-            lg
-            alignItems={matchesMD ? "center" : "flex-end"}
-          >
-            <Grid item>
-            {/** 300 for phones **/}
-              <img                
-                src="/assets/herbs.svg"
-                alt="imagalt"
-                style={{ maxWidth: matchesMD ? 300 : undefined }} 
-              />
-            </Grid>
-            <Grid item>
-              <Typography variant="caption">
-                MD Down brief descript caption flex end
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
       <Grid item>
         <CallToAction setValue={props.setValue} />
       </Grid>
